30ac15b68649f278fee64d264b1b32db11203aaf
Haider Majid <92584858+haider-majid@users.noreply.github.com>
2025-01-18 22:50:12 +0300

    - upload image book

---
+++ Migrations/20250118194053_addimage.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace books.Migrations
+{
+    /// <inheritdoc />
+    public partial class addimage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "imageUrl",
+                table: "Books",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "imageUrl",
+                table: "AddBookModel",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "imageUrl",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "imageUrl",
+                table: "AddBookModel");
+        }
+    }
+}
---
+++ Migrations/20250118194733_imageadd.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace books.Migrations
+{
+    /// <inheritdoc />
+    public partial class imageadd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AddBookModel");
+
+            migrationBuilder.RenameColumn(
+                name: "imageUrl",
+                table: "Books",
+                newName: "ImagePath");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "Books",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImagePath",
+                table: "Books",
+                newName: "imageUrl");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "Books",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AddBookModel",
+                columns: table => new
+                {
+                    author = table.Column<string>(type: "text", nullable: false),
+                    categoryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    imageUrl = table.Column<string>(type: "text", nullable: true),
+                    name = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+    }
+}
